a62b5d5eac47c289ba0349e21b1a162b4af2e305
Ashish <shirsalkarashishprakash@gmail.com>
2022-01-04 11:36:00 +0530

    final touch v2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=8000 learning_log.wsgi:application
+web: waitress-serve --port=$PORT learning_log.wsgi:application
--- learning_log/settings.py
+++ learning_log/settings.py
@@ -22,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'django-insecure-erc2&$ccl#6%=ha%g5zgl6!sdcitn!kg$+3n)o!v*1c#q13&^b'
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'django-insecure-erc2&$ccl#6%=ha%g5zgl6!sdcitn!kg$+3n)o!v*1c#q13&^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ["learnlogdata.herokuapp.com",'localhost']
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["learnlogdata.herokuapp.com",'localhost']
+#ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -135,9 +135,9 @@ LOGIN_URL = '/usersaccounts/login/'
 
 
 
-# STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR/'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
